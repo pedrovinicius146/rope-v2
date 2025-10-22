6ac13c849f920e6a1fdfb8c02cede6880c366343
Pedro Vinicius Curvelo De Araujo <pedrokan14@gmail.com>
2025-10-21 22:28:22 -0300

    railways v45

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,12 +8,7 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash da senha antes de salvar
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+
 
 // Comparar senha
 userSchema.methods.matchPassword = async function(password) {
